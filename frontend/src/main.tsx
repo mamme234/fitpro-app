5477f58f944a9b997b79613d48a975bf0b5f030c
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-02 15:47:41 +0300

    Initialize main.tsx with React Router setup

---
+++ frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./pages/App";
+import Login from "./pages/Login";
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
