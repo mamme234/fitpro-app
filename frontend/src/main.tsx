402154b034b57afaaa66bc51ecdd3b0f2b5c6547
mamme234 <ghazimuhammadilyas@gmail.com>
2026-07-02 15:59:45 +0300

    Add CSS import to main.tsx
    Import global CSS styles for the application.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 import Login from "./pages/Login";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
